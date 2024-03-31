9889e0dde72bd7be5e04511c8af9713a3a08af8e
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-03-30 23:24:26 -0400

    Task 1 Completed

--- Task-1.py
+++ Task-1.py
@@ -1,1 +1,13 @@
 # Python List Transformation
+
+grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
+grades.sort()
+grades.reverse()
+
+# average = sum(grades) / len(grades) returns a decimal 
+average = sum(grades) // len(grades)
+print(average)
+
+grades = grades[0:7]
+failure = ["Failed"] * 3
+print(grades + failure)
--- Task-2.py
+++ Task-2.py
@@ -1,1 +1,6 @@
 # Advanced List Methods and Identity Operators
+
+submitted = ["Alice", "Bob", "Charlie", "David"]
+attended = ["Charlie", "Eve", "Alice", "Frank"]
+
+print(submitted is attended)

60242824108309a30c6d21ef8d842cb6648b50f1
Tameka Roberson <TTRoberson@student.fullsail.edu>
2024-04-01 15:39:34 -0400

    Help with Task-2

--- Task-2.py
+++ Task-2.py
@@ -3,4 +3,19 @@
 submitted = ["Alice", "Bob", "Charlie", "David"]
 attended = ["Charlie", "Eve", "Alice", "Frank"]
 
-print(submitted is attended)
+# Possible Solution via Nested Loops
+for sub_name in submitted:
+    for att_name in attended:
+       if sub_name is att_name:
+           print(sub_name + " had submitted their work")
+           break
+    
+print(True if submitted is attended else False)
+
+# Help
+for att_names in attended:
+    for sub_names in submitted:
+        if att_names not in  sub_names:
+            removed = attended.remove(att_names)
+            print(removed)
+        
